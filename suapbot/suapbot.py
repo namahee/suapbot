c8b550f29c8ff813618e7552e5bfbc0ac31df14e
namahee <justheroku0@gmail.com>
2024-11-13 17:43:55 -0300

    inferno

--- suapbot/suapbot.py
+++ suapbot/suapbot.py
@@ -131,6 +131,7 @@ def _notas(disciplina_escolhida, disciplinas):
                 if n3 == None and n4 == None:
                     n2 = 0 if n2 == "nota não lançada" else n2
                     n1 = n1.replace(",", ".")
+                    n2 = n2.replace(",", ".")
                     an = (float(n1) + n2)
                     ps = round(14 - an, 2)
                     approved = "Você já foi aprovado na disciplina" if an >= 14 else f"Faltam {ps} pontos para você ser aprovado!"
@@ -155,7 +156,7 @@ Média final: {md}
 {approved}`"""
                 else:
                     n4 = 0 if n4 == "nota não lançada" else n4
-                    n1, n2, n3 = n1.replace(",", "."), n2.replace(",", "."), n3.replace(",", ".")
+                    n1, n2, n3, n4 = n1.replace(",", "."), n2.replace(",", "."), n3.replace(",", "."), n4.replace(",", ".")
                     an = (float(n1) + float(n2) + float(n3) + n4)
                     ps = round(28 - an, 2)
                     approved = "Você já foi aprovado na disciplina" if an >= 28 else f"Faltam {ps} pontos para você ser aprovado!"
