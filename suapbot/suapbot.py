10fbed65bc53de8032986e6d3593db9dfac7b696
namahee <justheroku0@gmail.com>
2024-11-13 17:31:54 -0300

    bucveta

--- suapbot/suapbot.py
+++ suapbot/suapbot.py
@@ -133,7 +133,7 @@ def _notas(disciplina_escolhida, disciplinas):
                         n2 = 0
                         n1 = n1.replace(",", ".")
                         an = (float(n1) + n2)
-                        ps = 28 - an
+                        ps = 14 - an
                         approved = "Você já foi aprovado na disciplina" if an >= 14 else f"Faltam {ps} pontos para você ser aprovado!"
                     boletim = f"""
 `Disciplina: {disciplina_escolhida.strip()}
@@ -157,8 +157,7 @@ Média final: {md}
                 else:
                     if n4 == "nota não lançada":
                         n4 = 0
-                        for i in [n1, n2, n3]:
-                            i = i.replace(",", ".")
+                        n1, n2, n3 = n1.replace(",", "."), n2.replace(",", "."), n3.replace(",", ".")
                         an = (float(n1) + float(n2) + float(n3) + n4)
                         ps = 28 - an
                         approved = "Você já foi aprovado na disciplina" if an >= 28 else f"Faltam {ps} pontos para você ser aprovado!"
