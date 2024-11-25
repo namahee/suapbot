849bcd38c1d287201319004179e0eb0547b7a056
namahee <85374690+namahee@users.noreply.github.com>
2024-11-25 15:17:09 -0300

    Update __main__.py

--- suapbot/__main__.py
+++ suapbot/__main__.py
@@ -1,4 +1,18 @@
 from client import b
+from fastapi import FastAPI
 
 if __name__ == "__main__":
-	b.run()
+	app = FastAPI()
+	@app.on_event("startup")
+	async def start_bot():
+	    print("Iniciando o bot...")
+	    await b.start()
+	
+	@app.on_event("shutdown")
+	async def stop_bot():
+	    print("Parando o bot...")
+	    await b.stop()
+	
+	@app.get("/")
+	def read_root():
+	    return {"message": "Bot Pyrogram está rodando no Vercel!"}
